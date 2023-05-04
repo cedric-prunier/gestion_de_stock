527cf04f1ea2b94b465af947ba64dd8b0e4eb996
cedric-prunier <cedric-prunier@laplateforme.io>
2023-05-04 13:31:49 +0200

    premier

---
+++ gestion_de_stock.py
@@ -0,0 +1,228 @@
+""""
+
+Commandes terminal
+mysql -u root -p 
+
+CREATE DATABASE boutique;
+
+USE boutique;
+
+CREATE TABLE categorie (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nom VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE produit (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nom VARCHAR(255) NOT NULL,
+  description TEXT,
+  prix INT NOT NULL,
+  quantité INT NOT NULL,
+  id_catégorie INT,
+  FOREIGN KEY (id_catégorie) REFERENCES categorie(id)
+);
+
+INSERT INTO categorie (nom) VALUES 
+('Électronique'),
+('Vêtements'),
+('Alimentation'),
+('Livres'),
+('Jouets');
+
+INSERT INTO produit (nom, description, prix, quantité, id_catégorie) VALUES 
+('Téléphone', 'Téléphone portable 5G', 800, 100, 1),
+('Ordinateur portable', 'Ordinateur portable 15 pouces', 1200, 50, 1),
+('T-shirt', 'T-shirt en coton', 20, 200, 2),
+('Pantalon', 'Pantalon en jean', 50, 150, 2);
+
+INSERT INTO produit (nom, description, prix, quantité, id_catégorie) VALUES 
+('Pâtes', 'Pâtes alimentaires 500g', 2, 300, 3),
+('Riz', 'Riz basmati 1kg', 4, 200, 3);
+
+INSERT INTO produit (nom, description, prix, quantité, id_catégorie) VALUES 
+('Roman policier', 'Un roman policier passionnant', 10, 100, 4),
+('Livre de cuisine', 'Recettes pour cuisiner à la maison', 15, 50, 4);
+
+INSERT INTO produit (nom, description, prix, quantité, id_catégorie) VALUES 
+('Puzzle', 'Puzzle 1000 pièces', 20, 30, 5),
+('Peluche', 'Peluche en forme d\'ours', 25, 40, 5);
+
+Modifier type de données requises:
+ALTER TABLE produit
+MODIFY prix FLOAT(10,2) NOT NULL;
+
+
+"""
+
+import mysql.connector
+
+
+class Gestion_stock:
+    def __init__(self):
+        self.db = mysql.connector.connect(
+            host="localhost", user="root", password="Egypte3813", database="boutique"
+        )
+        self.cursor = self.db.cursor()
+
+    def get_produit(self, nom):
+        query = "SELECT * FROM produit WHERE nom = %s"
+        values = nom
+        self.cursor.execute(query, values)
+        result = self.cursor.fetchone()
+        return result
+
+    def list_produit(self):
+        query = "SELECT * FROM produit"
+        self.cursor.execute(query)
+        return self.cursor.fetchall()
+
+    def ajouter_produit(self, nom, description, prix, quantité, id_catégorie):
+        query = "INSERT INTO produit (nom, description, prix, quantité, id_catégorie) VALUES (%s, %s, %s, %s, %s)"
+        values = (nom, description, prix, quantité, id_catégorie)
+        self.cursor.execute(query, values)
+        self.db.commit()
+
+    def update_produit(
+        self,
+        nom,
+        new_nom=None,
+        new_description=None,
+        new_prix=None,
+        new_quantité=None,
+        new_id_catégorie=None,
+    ):
+        query = "SELECT * FROM produit WHERE nom = %s"
+        self.cursor.execute(query, (nom,))
+        produit = self.cursor.fetchone()
+
+        if produit:
+            if new_nom is None:
+                new_nom = nom
+            if new_description is None:
+                new_description = produit[2]
+            if new_prix is None:
+                new_prix = produit[3]
+            if new_quantité is None:
+                new_quantité = produit[4]
+            if new_id_catégorie is None:
+                new_id_catégorie = produit[5]
+
+            query = "UPDATE produit SET nom = %s, description = %s, prix = %s, quantité = %s, id_catégorie = %s WHERE nom = %s"
+            values = (
+                new_nom,
+                new_description,
+                new_prix,
+                new_quantité,
+                new_id_catégorie,
+                nom,
+            )
+            self.cursor.execute(query, values)
+            self.db.commit()
+            return True
+        else:
+            return False
+
+    def supprimer_produit(self, nom):
+        query = "DELETE FROM produit WHERE nom = %s"
+        values = (nom,)
+        self.cursor.execute(query, values)
+        self.db.commit()
+
+
+def modifier_produit(bd_boutique):
+    nom = input("Saisir le nom du produit à modifier: ")
+    new_nom_input = input("Saisir le nouveau nom (laisser vide pour ne pas changer): ")
+    new_nom = new_nom_input if new_nom_input else None
+
+    new_description_input = input(
+        "Saisir la nouvelle description du produit (laisser vide pour ne pas changer): "
+    )
+    new_description = new_description_input if new_description_input else None
+
+    new_prix_input = input(
+        "Saisir le nouveau prix (laisser vide pour ne pas changer): "
+    )
+    new_prix = float(new_prix_input) if new_prix_input else None
+
+    new_quantité_input = input(
+        "Saisir la nouvelle quantité (laisser vide pour ne pas changer): "
+    )
+    new_quantité = int(new_quantité_input) if new_quantité_input else None
+
+    new_id_catégorie_input = input(
+        "Saisir le nouvel ID de catégorie (laisser vide pour ne pas changer): "
+    )
+    new_id_catégorie = int(new_id_catégorie_input) if new_id_catégorie_input else None
+
+    if new_nom or new_description or new_prix or new_quantité or new_id_catégorie:
+        updated = bd_boutique.update_produit(
+            nom,
+            new_nom=new_nom,
+            new_description=new_description,
+            new_prix=new_prix,
+            new_quantité=new_quantité,
+            new_id_catégorie=new_id_catégorie,
+        )
+        if updated:
+            print(f"Le produit '{nom}' a été mis à jour.")
+        else:
+            print(f"Aucun produit trouvé avec le nom '{nom}'.")
+    else:
+        print("Aucune modification fournie.")
+
+
+def ajouter_produit(bd_boutique):
+    nom = input("Entrez le nom du produit : ")
+    description = input("Entrez la description du produit : ")
+    prix = float(input("Entrez le prix du produit : "))
+    quantité = int(input("Entrez la quantité de stock : "))
+    id_catégorie = int(input("Entrez l'id de catégorie : "))
+    bd_boutique.ajouter_produit(
+        nom,
+        description,
+        prix,
+        quantité,
+        id_catégorie,
+    )
+
+    print("Le produit a été ajoutée avec succès.")
+
+
+def supprimer_produit(bd_boutique):
+    nom = input("Entrez le nom du produit : ")
+
+    bd_boutique.supprimer_produit(nom)
+
+    print("Le produit a été supprimé avec succès.")
+
+
+def show_menu():
+    print("Menu:")
+    print("1. Afficher la liste des produits")
+    print("2. Ajouter un produit")
+    print("3. Supprimer un produit")
+    print("4. Modifier un produit")
+    print("5. Quitter")
+
+
+def handle_menu_choice(bd_boutique, choice):
+    if choice == 1:
+        produit = bd_boutique.list_produit()
+        for produit in produit:
+            print(produit)
+    elif choice == 2:
+        ajouter_produit(bd_boutique)
+    elif choice == 3:
+        supprimer_produit(bd_boutique)
+    elif choice == 4:
+        modifier_produit(bd_boutique)
+    elif choice == 5:
+        quit()
+
+
+bd_boutique = Gestion_stock()
+
+while True:
+    show_menu()
+    choice = int(input("Veuillez choisir une option : "))
+    should_continue = handle_menu_choice(bd_boutique, choice)
